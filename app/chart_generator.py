3e0785903fcb67f66fcae3b196542538bbc95f0a
José Melo <jos.ryan.melo@gmail.com>
2026-07-28 20:06:31 -0300

    feat: add model and Chart Spec

--- app/chart_generator.py
+++ app/chart_generator.py
@@ -2,12 +2,19 @@ import os
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 
-from prompt import chart_prompt
+from app.prompt import chart_prompt
 from app.models.chart_spec import ChartSpec
 
 load_dotenv()
 
-model: str = ""
+model: str = "ministral-3b-latest"
+#model="ministral-3b-latest"
+#model="ministral-8b-latest"
+#model="mistral-small-latest"
+#model="mistral-medium-latest"
+#model="mistral-large-latest"
+
+
 temperature: float = 0.2
 max_tokens: int = 800
 
@@ -23,7 +30,7 @@ structured_llm = llm.with_structured_output(ChartSpec)
 
 chain = chart_prompt | structured_llm
 
-def generate_chart(question: str):
+def generate_chart(question: str) -> ChartSpec:
     return chain.invoke(
         {
             "question": question
