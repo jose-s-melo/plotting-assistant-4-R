d4e8310144639a973caadf2809a93a2b32a5f806
José Melo <jos.ryan.melo@gmail.com>
2026-07-28 18:32:13 -0300

    feat: add chart generator

---
+++ app/chart_generator.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+
+from prompt import chart_prompt
+from models.chart_spec import ChartSpec
+
+load_dotenv()
+
+model: str = ""
+temperature: float = 0.2
+max_tokens: int = 800
+
+
+llm = ChatMistralAI(
+    model=model,
+    max_tokens=max_tokens,
+    temperature=temperature,
+    api_key=os.getenv("MISTRAL_API_KEY")
+)
+
+structured_llm = llm.with_structured_output(ChartSpec)
+
+chain = llm | structured_llm
+
+def generate_chart(question: str):
+    return chain.invoke(
+        {
+            "question": question
+        }
+    )
